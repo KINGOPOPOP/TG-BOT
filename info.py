4043507ec3ba54da13de0f83c70e7404e9e3fcf3
kingxseller <125795142+kingxseller@users.noreply.github.com>
2023-12-26 15:11:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://king-aws9.onrender.com/".format(FQDN)
+    URL = "https://king-bot-futt.onrender.com/".format(FQDN)
 else:
-    URL = "https://king-aws9.onrender.com/".format(FQDN)
+    URL = "https://king-bot-futt.onrender.com/".format(FQDN)
 
 
 
